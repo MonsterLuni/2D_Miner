637dfe3cdf64278b897eddb5fe0f6f2009489079
MonsterLuni <monsterluni.specific@gmail.com>
2024-01-04 18:37:08 +0100

    All Hitboxes now work. (top is a bit buggy)

--- src/game/Player.java
+++ src/game/Player.java
@@ -9,8 +9,12 @@ public class Player {
     public int Y;
     public int offsetX = 0;
     public int offsetY = 0;
-    public int speed = 5;
+    public int walkSpeed = 5;
+    public int jumpSpeed = 10;
+    public int gravitySpeed = 5;
     public int defaultHeight = 50;
+    public int defaultWidth = 20;
+    public int width = defaultWidth;
     public int height = defaultHeight;
     KeyListener kh;
     UI ui;
@@ -22,12 +26,12 @@ public class Player {
     }
     public void drawPlayer(Graphics g){
         g.setColor(Color.blue);
-        g.fillRect(X,Y,20,height);
+        g.fillRect(X,Y,width,height);
     }
     public void gravity(){
         int index = ui.map.getBlockFromPlayerY((((X - offsetX) / 25) * 25),(((Y + offsetY) / 25) * 25));
         if (index == -10 || Y + offsetY + 5 < ui.blocks.get(index).Y - (height + Y) ) {
-            offsetY += 5;
+            offsetY += gravitySpeed;
         }
         else {
             offsetY = ui.blocks.get(ui.map.getBlockFromPlayerY((((X - offsetX) / 25) * 25), (((Y + offsetY) / 25) * 25))).Y - (height + Y);
@@ -35,37 +39,52 @@ public class Player {
     }
     public void jump(){
         if(kh.spacePressed){
-            offsetY -= 10;
+            int index = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25),(((Y + offsetY) / 25) * 25) + (height - 50));
+            int index2 = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25),(((Y + offsetY) / 25) * 25) + (height - 25));
+            if(ui.blocks.get(index2).hitBottom){
+                if(Y - offsetY + 5 <= ui.blocks.get(index).Y){
+                    System.out.println("left");
+                }
+                else {
+                    offsetY -= jumpSpeed;
+                }
+            }
+            else {
+                offsetY -= jumpSpeed;
+            }
         }
+    }
+    public boolean checkOverlapX(int index, int overlap, boolean right){
+        if(right){
+            return X - offsetX - overlap <= ui.blocks.get(index).X;
+        }
+        else {
+            return X - offsetX - overlap >= ui.blocks.get(index).X;
+        }
+
     }
     public void walk(){
         if(kh.aPressed){
             int index = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25),(((Y + offsetY) / 25) * 25) + (height - 25));
             int index2 = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25) - 25,(((Y + offsetY) / 25) * 25) + (height - 25));
             if(ui.blocks.get(index2).hitRight){
-                if(X - offsetX - 1 <= ui.blocks.get(index).X){
-                    System.out.println("left");
-                }
-                else {
-                    offsetX += speed;
+                if(checkOverlapX(index,1,false)){
+                    offsetX += walkSpeed;
                 }
             }
             else {
-                offsetX += speed;
+                offsetX += walkSpeed;
             }
         } else if (kh.dPressed) {
             int index = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25),(((Y + offsetY) / 25) * 25) + (height - 25));
             int index2 = ui.map.getBlockFromCoordinates((((X - offsetX) / 25) * 25) + 25,(((Y + offsetY) / 25) * 25) + (height - 25));
             if(ui.blocks.get(index2).hitLeft){
-                if(X - offsetX - 5 >= ui.blocks.get(index).X){
-                    System.out.println("right");
-                }
-                else {
-                    offsetX -= speed;
+                if(checkOverlapX(index,0,true)){
+                    offsetX -= walkSpeed;
                 }
             }
             else {
-                offsetX -= speed;
+                offsetX -= walkSpeed;
             }
         }
     }
